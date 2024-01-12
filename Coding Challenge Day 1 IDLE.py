bebe3a91649b29b5ace0ca729a4502a3d0e837f8
StevieDan <107366256+elizaheidebrecht@users.noreply.github.com>
2024-01-12 14:37:38 -0500

    Add files via upload

---
+++ Coding Challenge Day 1 IDLE.py
@@ -0,0 +1,19 @@
+#Create a program that swaps 
+#the values of two variables.
+
+#User prompted to enter 2 values
+value1 = input("Enter first value:")
+value2 = input("Enter second value:")
+
+#Print Initial Values
+print('\n',"Initial Value 1 =", value1,'\n',"Initial Value 2 =", value2)
+
+
+#Values are swapped
+placeholder_value = value1
+value1 = value2
+value2 = placeholder_value
+
+
+#Prints swapped values
+print('\n',"New Value 1 =", value1,'\n', "New Value 2 =", value2)
