0b69b99078788d13dd938dc4761b458512c75df0
LvvGvn <107366256+elizaheidebrecht@users.noreply.github.com>
2024-01-20 15:49:31 -0500

    Add files via upload

---
+++ Coding Challenge Day 9.py
@@ -0,0 +1,9 @@
+#Write a program to check if a number is even or odd.
+
+even_or_odd = float(input("Enter a number: "))
+
+#Find if number is even or odd
+if even_or_odd % 2 == 0:
+    print("Your number is even.")
+if even_or_odd % 2 == 1:
+    print("Your number is odd.")
