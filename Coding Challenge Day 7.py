9f55e765a03cb57cd82e81d61c467cc2e9b4295a
LvvGvn <107366256+elizaheidebrecht@users.noreply.github.com>
2024-01-20 12:44:48 -0500

    Add files via upload

---
+++ Coding Challenge Day 7.py
@@ -0,0 +1,10 @@
+#Write a program to check if a number is positive, negative, or zero.
+
+num_input = float(input("Enter a number: "))
+
+if num_input > 1:
+    print("This number is positive.")
+if num_input < 0:
+    print("This number is negative.")
+if num_input == 0:
+    print("This number is 0.")
