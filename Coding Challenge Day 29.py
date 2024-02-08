8768d417304a7b1c7470ad7d3d1113ca5e89cec1
LvvGvn <107366256+elizaheidebrecht@users.noreply.github.com>
2024-02-07 21:28:04 -0500

    Add files via upload

---
+++ Coding Challenge Day 29.py
@@ -0,0 +1,11 @@
+#Create a function that generates a random number between a given range.
+
+from random import randrange
+
+input_range = int(input("Enter the highest number of the range you want to generate:"))
+
+input_howmany = int(input("How many number do you want to generate?"))
+
+for i in range(input_howmany):
+    print(randrange(input_range))
+
